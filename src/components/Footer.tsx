e0d60f4fa8f79dc04d9b9185cb8c057997965d8a
Manuel Rodriguez Osuna <manrodri92@gmail.com>
2022-10-19 01:39:01 +0200

    ci: added license and deployment to gh (#6)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,13 @@ export const Footer = () => {
     return (
         <footer className="p-4 shadow md:px-6 md:py-8 bg-gray-900">
             <span className="block text-sm  sm:text-center text-gray-400">
-                © Manuel Rodriguez - 2022 - All Rights Reserved.
+                © Manuel Rodriguez - 2022 - All Rights Reserved.{' '}
+                <a
+                    className="ml-4 font-bold"
+                    href="https://github.com/manucho007"
+                >
+                    Github Manuel
+                </a>
             </span>
         </footer>
     )
